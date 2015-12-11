09537d48909f3104fc984cf46f32da30c2dcc909
jamalping <jamalping@shenji.local>
2015-12-11 23:27:21 +0800

    循环滚蛋视图--Swift

--- CycleScrollView/ViewController.swift
+++ CycleScrollView/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
+        let tap = UITapGestureRecognizer.init(target: self, action: "tapAction:")
+        self.view.addGestureRecognizer(tap)
+        self.view.tag = 1
+        
+        let cycleScrollView = CycleScrollView.init(frame: CGRectMake(0, 100, self.view.width, 300), animationTimerInterVal: 2)
+        cycleScrollView.dataSource = ["1","2","3"]
+        self.view.addSubview(cycleScrollView)
+        
+    }
+    func tapAction(tap:UITapGestureRecognizer) {
+        
+        print(tap.view?.tag)
     }
 
     override func didReceiveMemoryWarning() {
